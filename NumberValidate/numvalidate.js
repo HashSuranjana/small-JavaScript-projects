2f8acc2affb46df8ee0b09bff3753427ac104e63
HashSuranjana <hasithasuranjana12@gmail.com>
2024-04-16 14:58:46 +0530

    ValidateNumber Completed

---
+++ NumberValidate/numvalidate.js
@@ -0,0 +1,31 @@
+function ValidateNum() {
+    
+    let number = document.getElementById('num-input').value ; //getting number from input box and declare it to number variable
+
+    if ( number.length < 9){
+        
+        document.getElementById('message').innerHTML = "Invalid Number !" ;
+    }
+
+    else if (number.length == 9 && number[0] == 7) {
+        
+        document.getElementById('message').innerHTML = "+94" + number ;
+    }
+
+    else if (number.length == 10 && number[0]== 0) {
+        
+        let newNumber = number.slice(1,-1) ;
+        document.getElementById('message').innerHTML = "+ 94" + newNumber ;
+    }
+
+    else if (number.length == 11 && (number[0]==9 && number[1]==4) ){
+
+        document.getElementById('message').innerHTML = "+ " + number ;
+    }
+
+    else{
+
+        document.getElementById('message').innerHTML = "Invalid Number !" ;
+    }
+
+}
