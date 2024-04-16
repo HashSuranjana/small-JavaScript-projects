1179acfb7339b5931b74d6dff97f7e8f57212757
HashSuranjana <hasithasuranjana12@gmail.com>
2024-04-16 10:13:41 +0530

    updated files

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,44 +1,54 @@
 let operation = 0 ;
 
 function fn0(){
-    let value = document.getElementById('screen').innerHTML= 0;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 0 ;
 
 }
 
 function fn1(){
-    let value = document.getElementById('screen').innerHTML = 1;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 1 ;
 }
 
 function fn2(){
-    let value = document.getElementById('screen').innerHTML = 2;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 2 ;
 }
 
 function fn3(){
-    let value = document.getElementById('screen').innerHTML = 3;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 3 ;
 }
 
 function fn4(){
-    let value = document.getElementById('screen').innerHTML = 4;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 4 ;
 }
 
 function fn5(){
-    let value = document.getElementById('screen').innerHTML = 5;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 5 ;
 }
 
 function fn6(){
-    let value = document.getElementById('screen').innerHTML = 6;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 6 ;
 }
 
 function fn7(){
-    let value = document.getElementById('screen').innerHTML = 7;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 7 ;
 }
 
 function fn8(){
-    let value = document.getElementById('screen').innerHTML = 8;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 8 ;
 }
 
 function fn9(){
-    let value = document.getElementById('screen').innerHTML = 9;
+    let value = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = value + 9 ;
 }
 
 
