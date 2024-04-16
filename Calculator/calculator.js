b625d1f3ee493c6e9e5ac4808c267e45a76fd6fa
HashSuranjana <hasithasuranjana12@gmail.com>
2024-04-16 09:58:17 +0530

    updated files

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,3 +1,42 @@
+
+
 function fn0(){
-    
+    let value = document.getElementById('screen').innerHTML=0;
+
+}
+
+function fn1(){
+    let value = document.getElementById('screen').innerHTML = 1;
+}
+
+function fn2(){
+    let value = document.getElementById('screen').innerHTML = 2;
+}
+
+function fn3(){
+    let value = document.getElementById('screen').innerHTML = 3;
+}
+
+function fn4(){
+    let value = document.getElementById('screen').innerHTML = 4;
+}
+
+function fn5(){
+    let value = document.getElementById('screen').innerHTML = 5;
+}
+
+function fn6(){
+    let value = document.getElementById('screen').innerHTML = 6;
+}
+
+function fn7(){
+    let value = document.getElementById('screen').innerHTML = 7;
+}
+
+function fn8(){
+    let value = document.getElementById('screen').innerHTML = 8;
+}
+
+function fn9(){
+    let value = document.getElementById('screen').innerHTML = 9;
 }
