9616206667cb0e5cf0fe64c0ec5615e8e47c4c7a
HashSuranjana <hasithasuranjana12@gmail.com>
2024-04-19 00:38:28 +0530

    Styling is to be added

--- BlackJackGame/BlackJack.js
+++ BlackJackGame/BlackJack.js
@@ -1,11 +1,13 @@
 let total = 0 ;
 
+let image ;
+
 document.getElementById('total').innerHTML = "Current Total is : 0 " ;
 
 
 function GenCard() {
 
-    document.getElementById('card-preview').style.visibility = "visible" ;
+    image = document.createElement('img') ;
 
     let min_val = 1 ;   // initialize a minimum value that user will get randomly
     let max_val = 13 ;  // initialize a maximum value that user will get randomly
@@ -13,119 +15,120 @@ function GenCard() {
     let number = Math.floor(Math.random() * (max_val - min_val + 1)) + min_val;
     console.log(number);
 
-    
-    if ( total > 21 ) {
-        
-        document.getElementById('message').innerHTML = "Game Over ! You Lost !!!!" ;
+    total += number ; 
 
-    }
 
-    else if ( total == 21) {
-        
-        document.getElementById('message').innerHTML = " Congratulations ! You Won !!!!" ;
+    if ( total <= 21) {
 
-    }
-
-    else {
+        document.getElementById('total').innerHTML = "Current Total is : " + total ;
 
         switch(number) {
-        
+    
             case 1 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/AH.svg";
+                image.src = "Images/AH.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 2 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/2H.svg";
+                image.src = "Images/2H.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 3 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/3H.svg";
+                image.src = "Images/3H.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 4 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/4H.svg";
+                image.src = "Images/4H.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 5 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/5H.svg";
+                image.src = "Images/5H.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 6 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/6H.svg";
+                image.src = "Images/6H.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 7 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/7H.svg";
+                image.src = "Images/7H.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 8 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/8H.svg";
+                image.src = "Images/8H.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 9 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/9H.svg";
+                image.src = "Images/9H.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 10 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/TH.svg";
+                image.src = "Images/TH.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 11 :
-    
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/JH.svg";
+        
+                image.src = "Images/JH.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 12 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/QH.svg";
+                image.src = "Images/QH.svg";
+                document.body.appendChild(image);
                 break ;
     
             case 13 :
     
-                total += number ;
-                document.getElementById('total').innerHTML = "Current Total is : " + total ;
-                document.getElementById('card-preview').src = "Images/KH.svg";
+                image.src = "Images/KH.svg";
+                document.body.appendChild(image);
                 break ;
     
         }
+        
+        if(total == 21) {
+            document.getElementById('message').innerHTML = " Congratulations ! You Won !!!!" ;
+            document.getElementById('restart').style.visibility = "visible" ;
+            document.getElementById('generate-btn').style.visibility = "hidden" ;
+        }
+
     }
+
+
+    else if( total > 21 ) {
+        
+        document.getElementById('message').innerHTML = "Game Over ! You Lost !!!!" ;
+        document.getElementById('restart').style.visibility = "visible" ;
+        document.getElementById('generate-btn').style.visibility = "hidden" ;
+
+
+    }
+
 
 }
 
+// function to reload the game 
+
 function ReGenCard() {
+
+    location.reload() ;
 
 }
